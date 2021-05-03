457aaf095db16f714b09117772ee80ba6ddc3d42
Elad Ben David <eladbendavid7@gmail.com>
2021-05-03 23:47:45 +0300

    got the function (sin(x) + cos(x))**1/3 as function so we can derivative with the func diff(f(x)) ezpz

--- cubic_splines.py
+++ cubic_splines.py
@@ -1,13 +1,22 @@
 import math
 import matplotlib.pyplot as plt
 import numpy as np
-from sympy import symbols, diff
+from sympy import symbols, diff,sin,cos
 
-def z_calc():
 
-def c_calc():
+def f(x):
+    return (sin(x) + cos(x))**(1/3)
 
-def d_calc():
+# def z_calc():
+#
+# def c_calc():
+#
+# def d_calc():
 
 
 if __name__ == '__main__':
+    x = symbols('x')
+    # a = math.pi/2
+    fx = f(x)
+    dfx = diff(f(x))
+    print(f'fx is :{fx} dfx is {dfx}')
